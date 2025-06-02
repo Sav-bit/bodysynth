ae91d9635051c9e8bf665360b29c8304ad51be5d
Sav <sav.procopio@gmail.com>
2025-06-02 14:25:49 +0200

    Reduce the number of training epochs from 10,000 to 500

--- training/train.py
+++ training/train.py
@@ -210,7 +210,7 @@ if __name__ == "__main__":
     print(f"Using device: {device}")
 
     # Set static parameters
-    num_epochs = 10_000  # How many epochs to train
+    num_epochs = 500  # How many epochs to train
     batch_size = 2  # How many images to load at once
     num_batches_per_epoch = 100  # How many batches to load per epoch
     patch_size = [180, 180, 180]
